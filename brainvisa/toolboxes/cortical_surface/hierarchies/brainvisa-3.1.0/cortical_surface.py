b1443c6436019c844b6dbef808d62e578180acad
operto <operto@bioproj.invalid>
2009-10-13 12:17:26 +0000

    added hierarchy for Cortical Latitude/Longitude Textures and Curvature Blobs Graph/Texture
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/cortical_surface-gpl/trunk@35148 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/cortical_surface/hierarchies/brainvisa-3.1.0/cortical_surface.py
+++ brainvisa/toolboxes/cortical_surface/hierarchies/brainvisa-3.1.0/cortical_surface.py
@@ -1,34 +1,3 @@
-#  This software and supporting documentation are distributed by
-#      Institut Federatif de Recherche 49
-#      CEA/NeuroSpin, Batiment 145,
-#      91191 Gif-sur-Yvette cedex
-#      France
-#
-# This software is governed by the CeCILL license version 2 under
-# French law and abiding by the rules of distribution of free software.
-# You can  use, modify and/or redistribute the software under the 
-# terms of the CeCILL license version 2 as circulated by CEA, CNRS
-# and INRIA at the following URL "http://www.cecill.info". 
-#
-# As a counterpart to the access to the source code and  rights to copy,
-# modify and redistribute granted by the license, users are provided only
-# with a limited warranty  and the software's author,  the holder of the
-# economic rights,  and the successive licensors  have only  limited
-# liability.
-#
-# In this respect, the user's attention is drawn to the risks associated
-# with loading,  using,  modifying and/or developing or reproducing the
-# software by the user in light of its specific status of free software,
-# that may mean  that it is complicated to manipulate,  and  that  also
-# therefore means  that it is reserved for developers  and  experienced
-# professionals having in-depth computer knowledge. Users are therefore
-# encouraged to load and test the software's suitability as regards their
-# requirements in conditions enabling the security of their systems and/or 
-# data to be ensured and,  more generally, to use and operate it in the 
-# same conditions as regards security.
-#
-# The fact that you are presently reading this means that you have had
-# knowledge of the CeCILL license version 2 and that you accept its terms.
 include( 'base' )
 include( 'sulci' )
 
@@ -51,6 +20,10 @@ insert( '{protocol}/{subject}',
     '<subject>_R_lat', SetType( 'Right hemisphere latitude texture'), SetWeakAttr( 'side', 'right' ),
     '<subject>_L_lon', SetType( 'Left hemisphere longitude texture'), SetWeakAttr( 'side', 'left' ),
     '<subject>_R_lon', SetType( 'Right hemisphere longitude texture'), SetWeakAttr( 'side', 'right' ),
+    '<subject>_Lconf_lat', SetType( 'Conformal latitude texture'), SetWeakAttr( 'side', 'left' ),
+    '<subject>_Rconf_lat', SetType( 'Conformal latitude texture'), SetWeakAttr( 'side', 'right' ),
+    '<subject>_Lconf_lon', SetType( 'Conformal longitude texture'), SetWeakAttr( 'side', 'left' ),
+    '<subject>_Rconf_lon', SetType( 'Conformal longitude texture'), SetWeakAttr( 'side', 'right' ),
     '<subject>_rootsValues', SetType( 'Constraint coordinates values'),
     '<subject>_L_grid', SetType( 'Left hemisphere coordinate grid'), SetWeakAttr( 'side', 'left' ),
     '<subject>_R_grid', SetType( 'Right hemisphere coordinate grid'), SetWeakAttr( 'side', 'right' ),
@@ -82,6 +55,12 @@ insert( '{protocol}/{subject}',
     "<subject>_Rwhite_curv_blob", SetType( 'Blob White Curvature Texture' ), SetWeakAttr( 'side', 'right' ),
     "<subject>_Lwhite_curv_ss", SetType( 'Scale Space White Curvature Texture' ), SetWeakAttr( 'side', 'left' ),
     "<subject>_Rwhite_curv_ss", SetType( 'Scale Space White Curvature Texture' ), SetWeakAttr( 'side', 'right' ),
+    "<subject>_L_labels", SetType( 'Labeled Functional Blobs Texture' ), SetWeakAttr( 'side', 'left' ),
+    "<subject>_R_labels", SetType( 'Labeled Functional Blobs Texture' ), SetWeakAttr( 'side', 'right' ),
+    "<subject>_L_curv_blobs", SetType( 'Curvature Blobs Texture' ), SetWeakAttr( 'side', 'left' ),
+    "<subject>_R_curv_blobs", SetType( 'Curvature Blobs Texture' ), SetWeakAttr( 'side', 'right' ),
+    "<subject>_L_curv_blobs", SetType( 'Curvature Blobs Graph' ), SetWeakAttr( 'side', 'left' ),
+    "<subject>_R_curv_blobs", SetType( 'Curvature Blobs Graph' ), SetWeakAttr( 'side', 'right' )
   ),
 )
 
