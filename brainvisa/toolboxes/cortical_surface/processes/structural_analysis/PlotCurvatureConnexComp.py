acda044fea2958b0e63c70d1bec7f34f6280f2a1
operto <operto@bioproj.invalid>
2009-12-02 15:06:09 +0000

    wip on stats
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/cortical_surface-gpl/trunk@35810 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/cortical_surface/processes/structural_analysis/PlotCurvatureConnexComp.py
+++ brainvisa/toolboxes/cortical_surface/processes/structural_analysis/PlotCurvatureConnexComp.py
@@ -46,11 +46,20 @@ userLevel = 3
 signature = Signature(
     'white', ReadDiskItem( 'Hemisphere White Mesh', 'MESH mesh'),
     'scale_space', ReadDiskItem( 'Scale Space White Curvature Texture', 'Texture'),
-    'csv_file', WriteDiskItem('Text File', 'Text File')
+    'csv_file', WriteDiskItem('Text File', 'Text File'),
+    'gyri', ReadDiskItem( 'Hemisphere gyri parcellation texture', 'Texture')
     )
 
+def count_label_max(texture):
+  label_max = 0
+  for i in xrange(len(texture)):
+    if (texture[i] > label_max):
+      label_max = texture[i]
+  return label_max
+
 def initialization( self ):
-  pass
+  self.linkParameters( 'gyri', 'white' )
+  self.linkParameters( 'scale_space', 'white' )
 
 def execution( self, context ):
     sujet = self.white.get( 'subject')
@@ -59,23 +68,30 @@ def execution( self, context ):
     # on lit la texture d'espace echelle et on cree un fichier temporaire
     scale_space = aims.read(str(self.scale_space))
     curvature_tmp_texfile = context.temporary("Texture")
-    context.write( "temporary : " + str (curvature_tmp_texfile) )
+    context.write( "curvature_tmp_texfile : " + str (curvature_tmp_texfile) )
     nb_scales = scale_space.size()
     context.write( str(nb_scales) + " scales" )
 
+    #on calcule la texture d'intersection de gyri
+    intersection_texfile = context.temporary("Texture")
+    context.write("intersection_texfile : " + str(intersection_texfile))
+    command = [ 'AimsTextureLabelIntersection', '-m', self.white, '-t', self.gyri, '-o', str(intersection_texfile)]
+    apply(context.system, command)
+    intersection_texture = aims.read(str(intersection_texfile))
+
 
     data = []
-    data.append(["scale_number", "threshold", "cc"])
+    data.append(["sujet", "scale_number", "threshold", "cc", "nb_cc_inters", "nb_cc_noninters"])
     # pour chaque niveau d'echelle
     for scale_number in xrange(nb_scales) :
       context.write( "scale :" + str(scale_number) )
       threshold = 0.0
-      label_max = 1
+      nb_cc = 1
 
       all_thresholds_cc_tex = []
       iteration = 0
       
-      while (label_max != 0):
+      while (nb_cc != 0):
         
         # on recupere la texture du niveau d'echelle courant
         scale_texture = aims.TimeTexture_FLOAT(1,len(scale_space[scale_number]))
@@ -92,34 +108,55 @@ def execution( self, context ):
         # on convertit en FLOAT texture
         command = [ 'AimsFileConvert', '-i', str(curvature_tmp_texfile), '-o', str(curvature_tmp_texfile), '-t', 'FLOAT' ]
         apply(context.system, command)
-        
-        # on calcule les composantes connexes
+
+        # on calcule une premiere fois les composantes connexes
         command = [ 'AimsMeshConnectedComponent', '-t', str(curvature_tmp_texfile), '-i', self.white, '-o', str(curvature_tmp_texfile), '-m', '1', '-T', '0']
         apply(context.system, command)
 
-        ##on calcule un voronoi pour tester
-        #command = [ 'AimsTextureVoronoi', '-m', self.white, '-t', str(curvature_tmp_texfile), '-o', "/volatile/operto/voronoi.tex"]
-        #apply(context.system, command)
-
-        
         # on compte combien de composantes connexes
-        compconn_texture = aims.read(str(curvature_tmp_texfile))
+        compconned_texture_to_be_filtered = aims.read(str(curvature_tmp_texfile))
+        nb_cc = count_label_max(compconned_texture_to_be_filtered[0])
 
-        all_thresholds_cc_tex.append(compconn_texture)
+        # mais d'abord on filtre par la texture d'intersection
+        compconn = {}
         
-        label_max = 0
-        for i in xrange(len(compconn_texture[0])):
-          if (compconn_texture[0][i] > label_max):
-            label_max = compconn_texture[0][i]
+        for i in xrange(len(compconned_texture_to_be_filtered[0])):
+          label = compconned_texture_to_be_filtered[0][i]
+          if (label != -1):
+            if (compconn.has_key(label) is False):
+              compconn[label] = []
+            compconn[label].append(i)
+
+        nb_inters_labels = 0
+        nb_noninters_labels = 0
+        for label in compconn.keys():
+          is_one_inters = False
+          for node in compconn[label]:
+            if (intersection_texture[0][node] != 0):
+              is_one_inters = True
+          if (is_one_inters):
+            nb_inters_labels = nb_inters_labels + 1
+          else :
+            nb_noninters_labels = nb_noninters_labels + 1
+            # on efface la compconn de la texture
+            for node in compconn[label]:
+              compconned_texture_to_be_filtered[0][node] = -1
+
+        all_thresholds_cc_tex.append(compconned_texture_to_be_filtered)
+
+        context.write(str(nb_cc) + " " + str(nb_inters_labels) + " " + str(nb_noninters_labels))
 
         #context.write( "scale :" + str(scale_number) + " thresh : "+ str(threshold) +
            #" label_max : " + str (label_max) )
         
 
         item = []
+        item.append(sujet)
         item.append(scale_number)
         item.append(-threshold)
-        item.append(label_max)
+        item.append(nb_cc)
+        item.append(nb_inters_labels)
+        item.append(nb_noninters_labels)
         data.append(item)
         iteration = iteration + 1
 
