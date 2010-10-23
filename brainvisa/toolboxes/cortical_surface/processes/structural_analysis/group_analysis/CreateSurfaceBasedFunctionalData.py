fdc88cac3790fc44dd39f0823a2d5f476dc02a27
operto <operto@bioproj.invalid>
2010-10-23 16:51:33 +0000

    this update should make the links correct at last (in structural_analysis/CreateSurfaceBasedFunctionalData)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/cortical_surface-gpl/trunk@39640 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/cortical_surface/processes/structural_analysis/group_analysis/CreateSurfaceBasedFunctionalData.py
+++ brainvisa/toolboxes/cortical_surface/processes/structural_analysis/group_analysis/CreateSurfaceBasedFunctionalData.py
@@ -66,15 +66,20 @@ def initialization( self ):
 
     eNode.addChild( 'Kernels', ProcessExecutionNode( 'CreateKernels', optional = 1 ) )
     eNode.addChild( 'Projection', ProcessExecutionNode( 'ProjectionUsingKernels', optional = 1 ) )
-    eNode.addLink('Average.input','fmri_data')
-    eNode.addLink('Registration.source_image','white_mesh')
-    eNode.addLink('Registration.reference_image','Average.output')
-    eNode.addLink('MeshTransform.input','white_mesh')
-    eNode.addLink('Kernels.intmesh','white_mesh')
+
+    eNode.addLink('Projection.fMRI_4D_data', 'fmri_data')
+    eNode.addLink('Average.input', 'fmri_data')
+    eNode.addLink('Registration.source_image', 'white_mesh')
+    eNode.addLink('Registration.reference_image', 'Average.output')
+    eNode.addLink('MeshTransform.input', 'white_mesh')
+    eNode.addLink('Kernels.intmesh', 'white_mesh')
     eNode.addLink('Kernels.resolution','fmri_data',self.getResolution)
     eNode.addLink('timeserie_texture', 'fmri_data', self.getName)
     eNode.addLink('timeserie_texture', 'white_mesh', self.getName)
-    
+
+    eNode.addLink('Projection.fMRI_surface_data','timeserie_texture')
+
+
     outAver = defaultContext().temporary('Nifti-1 image')
     outTransform = defaultContext().temporary('MESH mesh')
     eNode.Average.setValue('output', outAver )
@@ -87,6 +92,7 @@ def initialization( self ):
     eNode.Registration.changeSignature( signat )
 
     eNode.addLink('Projection.white_mesh', 'MeshTransform.output')
+    eNode.removeLink('MeshTransform.output', 'MeshTransform.input')
 
     eNode.MeshTransform.setValue('output', outTransform)
     eNode.Projection.setValue('white_mesh', outTransform)
@@ -96,13 +102,8 @@ def initialization( self ):
 
     eNode.Projection.removeLink('kernels', 'white_mesh')
 
-    #eNode.Projection.removeLink('fmri', 'white_mesh')
-    
+    eNode.addLink('Projection.kernels', 'Kernels.output')
 
-    eNode.addLink('Projection.kernels','Kernels.output')
-    eNode.addLink('Projection.fMRI_4D_data','fmri_data')
-
-    eNode.addLink('Projection.fMRI_surface_data','timeserie_texture')
 
     self.setExecutionNode( eNode )
 
