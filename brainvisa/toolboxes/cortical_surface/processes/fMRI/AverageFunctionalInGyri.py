7e2208fbc7418cffbbdfaa2f03fdf7c84d0a0644
coulon <coulon@bioproj.invalid>
2010-10-29 18:25:24 +0000

    Added a process that averages functional signals in each gyri and returns a text file containing : gyrus1 label average signal for gyrus1 etc...
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/cortical_surface-gpl/trunk@39722 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ brainvisa/toolboxes/cortical_surface/processes/fMRI/AverageFunctionalInGyri.py
@@ -0,0 +1,81 @@
+#  This software and supporting documentation are distributed by
+#      Institut Federatif de Recherche 49
+#      CEA/NeuroSpin, Batiment 145,
+#      91191 Gif-sur-Yvette cedex
+#      France
+#
+# This software is governed by the CeCILL license version 2 under
+# French law and abiding by the rules of distribution of free software.
+# You can  use, modify and/or redistribute the software under the 
+# terms of the CeCILL license version 2 as circulated by CEA, CNRS
+# and INRIA at the following URL "http://www.cecill.info". 
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using,  modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean  that it is complicated to manipulate,  and  that  also
+# therefore means  that it is reserved for developers  and  experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or 
+# data to be ensured and,  more generally, to use and operate it in the 
+# same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license version 2 and that you accept its terms.
+
+from neuroProcesses import *
+import shfjGlobals     
+from soma import aims
+from numpy import *
+
+
+name = 'Average Functional information in Gyri'
+
+userLevel = 1
+
+signature = Signature(
+     'GyriTexture', ReadDiskItem( 'Texture', 'Texture' ), 
+     'FunctionalTexture',ReadDiskItem( 'Texture', 'Texture' ),
+     'ResultTextFile', WriteDiskItem( 'Text File', 'Text File')
+)
+
+def initialization( self ):
+
+def execution( self, context ): 
+     readerF = aims.Reader()
+     texF=readerF.read(str(self.Functionaltexture) )
+
+     ni=texF.nItem()
+     nt=texF.header()['nb_t_pos']
+
+     mat=array(texF[0])
+     for i in range(1,nt):
+          mat=vstack((mat, array(texF[i])))
+
+     matT=mat.transpose()
+
+     readerG = aims.Reader()
+     gyriTex=readerG.read(str(self.GyriTexture) )
+     gyri=array(gyriTex[0])
+     nlab=gyri.max()
+
+     fileO=open(str(self.ResultTextFile) ,'w')
+     for i in range(0, int(nlab+1)):
+          gyrus=matT[where( gyri==i )]
+          if (gyrus.size != 0):
+               print str(i)+' '
+               meanG=mean(gyrus, axis=0)
+               fileO.write(str(i)+'\n')
+               sig = ""
+               for j in meanG:
+                    sig = sig + str(j) + ' '
+               fileO.write(sig+'\n')
+
+     fileO.close()
