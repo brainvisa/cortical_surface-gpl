fdf7274e841a73cc2302b4d378bd9b43ede9d7c5
operto <operto@bioproj.invalid>
2011-01-20 16:16:10 +0000

    correcting a bug
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/cortical_surface-gpl/trunk@40600 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/cortical_surface/processes/structural_analysis/group_analysis/Structural Analysis/CreateResultsLabelsTexture.py
+++ brainvisa/toolboxes/cortical_surface/processes/structural_analysis/group_analysis/Structural Analysis/CreateResultsLabelsTexture.py
@@ -18,15 +18,16 @@ def initialization( self ):
   self.linkParameters("textures", "labelled_primalsketches")
 
 def execution( self, context ):
-    graphs = [aims.read(each) for each in self.labelled_primalsketches]
-    meshes = [ each['mesh'] for each in graphs ]
+    graphs = [aims.read(each.fullPath()) for each in self.labelled_primalsketches]
+    meshes = [ aims.read(each['mesh']) for each in graphs ]
 
     for j in xrange(len(self.labelled_primalsketches)):
         context.write(self.labelled_primalsketches[j])
         if (self.mode == 'all'):
-            texture = aims.TimeTexture_S16( len(self.labelled_primalsketches) + 1, meshes[j].vertex().size() )
+            #texture = aims.TimeTexture_S16( len(self.labelled_primalsketches) + 1, len(meshes[j].vertex()) )
+            texture = aims.TimeTexture_S16( 1, len(meshes[j].vertex()) )
         elif (self.mode == 'more than <threshold> subjects' or self.mode == 'over 95.% significance'):
-            texture = aims.TimeTexture_S16( 1, meshes[j].vertex().size() )
+            texture = aims.TimeTexture_S16( 1, len(meshes[j].vertex()) )
         for k in xrange( texture.size() ):
             for i in xrange( len(texture[k]) ):
                 texture[k][i] = 0
@@ -36,33 +37,35 @@ def execution( self, context ):
   
         for v in graphs[j].vertices():
             nodes_list = v['nodes']
-            label = int(v['label'])
+            #if v.has_key('label'):
+            #label = int(v['label'])
             for node in nodes_list:
                 texture[0][node] = 0
         for v in graphs[j].vertices():
-            if ( set(['label_occur_number', 'significance']).issubset(set(v.keys())) ) :
+            #if ( set(['label_occur_number', 'significance']).issubset(set(v.keys())) ) :
                 nodes_list = v['nodes']
-                label = int(v['label'])
-                lon = int(v['label_occur_number'])
-                signif = float(v['significance'])
-                ok = 0
-                if ( lon > len(self.labelled_primalsketches) ):
-                    lon = len(self.labelled_primalsketches)
-                if ( self.mode == 'more than <threshold> subjects' ):
-                    if ( lon > int(self.threshold) ):
-                        lon = 0
+                if v.has_key('label'):
+                    label = int(v['label'])
+                    #lon = int(v['label_occur_number'])
+                    #signif = float(v['significance'])
+                    ok = 0
+                    #if ( lon > len(self.labelled_primalsketches) ):
+                        #lon = len(self.labelled_primalsketches)
+                    #if ( self.mode == 'more than <threshold> subjects' ):
+                        #if ( lon > int(self.threshold) ):
+                            #lon = 0
+                            #ok = 1
+                    if ( self.mode == 'all' ):
                         ok = 1
-                elif ( self.mode == 'all' ):
-                    ok = 1
-                elif ( self.mode == 'over 95.% significance' ):
-                    if (signif > 95.0):
-                        lon = 0
-                        ok = 1
-                if ( ok == 1 and label > 0 ):
-                    for node in nodes_list:
-                        texture[lon][node] = int ( v['label_occur_number'] )
-                #print str(label) + " " + str(v['tValue'])  #+ " " + str(v['lifeTime']) + " " + str(v['t'])
-                nbblobsnonnuls = nbblobsnonnuls + 1
+                    elif ( self.mode == 'over 95.% significance' ):
+                        if (signif > 95.0):
+                            lon = 0
+                            ok = 1
+                    if ( ok == 1 and label > 0 ):
+                        for node in nodes_list:
+                            texture[0][node] = label #int ( v['label_occur_number'] )
+                    #print str(label) + " " + str(v['tValue'])  #+ " " + str(v['lifeTime']) + " " + str(v['t'])
+                    nbblobsnonnuls = nbblobsnonnuls + 1
 
         context.write("nb blobs non nuls : " + str(nbblobsnonnuls))
         context.write("Texture " + str(j))
